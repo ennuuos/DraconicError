c1ccf7b49ceac6e6d378a87d3e1670bbdcf4d2eb
Travis Valenti <ennuuos@gmail.com>
2017-02-19 23:16:17 +1100

    - Streamlined
    - Commented out unused classes
    - Fixed stupid issues

--- Classes.py
+++ Classes.py
@@ -1,11 +1,55 @@
 class Levels:
-    pass
+    def __init__(self):
 
-class Classtype:
-    def __init__(start):
+        self.Barbarian  = 0
+        self.Bard       = 0
+        self.Cleric     = 0
+        self.Druid      = 0
+        self.Fighter    = 0
+        self.Monk       = 0
+        self.Paladin    = 0
+        self.Ranger     = 0
+        self.Rogue      = 0
+        self.Sorcerer   = 0
+        self.Warlock    = 0
+        self.Wizard     = 0
 
-        self.level = 0
-
-
-class Barbarian(Classtype):
-    pass
+# class Classtype:
+#     def __init__(start):
+#         pass
+#
+# class Barbarian(Classtype):
+#     pass
+#
+# class Bard(Classtype):
+#     pass
+#
+# class Cleric(Classtype):
+#     pass
+#
+# class Druid(Classtype):
+#     pass
+#
+# class Fighter(Classtype):
+#     pass
+#
+# class Monk(Classtype):
+#     pass
+#
+# class Paladin(Classtype):
+#     pass
+#
+# class Ranger(Classtype):
+#     pass
+#
+# class Rogue(Classtype):
+#     pass
+#
+# class Sorcerer(Classtype):
+#     pass
+#
+# class Warlock(Classtype):
+#     pass
+#
+# class Wizard(Classtype):
+#     pass
--- Entity.py
+++ Entity.py
@@ -1,8 +1,10 @@
 from Attributes import Attributes
+from Health import Health
+from Transient import Transient
 
 class Entity:
     def __init__(self):
         self.attr = Attributes()
 
-        # self.health = Health()
-        # self.transient = Transient()
+        self.health = Health()
+        self.transient = Transient()
