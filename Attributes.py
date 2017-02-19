f18e1416bd99045796d14158dd9c37e2c1f20f96
Travis Valenti <ennuuos@gmail.com>
2017-02-19 21:44:59 +1100

    A bunch of shamizzle

--- Attributes.py
+++ Attributes.py
@@ -1,3 +1,4 @@
+
 class Attributes:
     def __init__(self):
         self.str = 10
@@ -6,3 +7,6 @@ class Attributes:
         self.int = 10
         self.wis = 10
         self.cha = 10
+    @staticmethod
+    def modifier(value):
+        return int(value / 2 - 5)
