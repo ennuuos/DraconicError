cab54831eba2c46b2e8368174135ca58a34d537c
Travis Valenti <ennuuos@gmail.com>
2017-02-19 21:12:28 +1100

    updated character.py

--- Character.py
+++ Character.py
@@ -1,5 +1,17 @@
 from CharacterApp import CharacterApp
-class Character:
+
+class Entity:
+    def __init__(self):
+
+        self.str = 10
+        self.dex = 10
+        self.con = 10
+        self.int = 10
+        self.wis = 10
+        self.cha = 10
+
+
+class Character(Entity):
     def __init__(self):
 
         self.health = None
